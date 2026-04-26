8a436654eecb55207b16696315ad76a95d4becb3
ManifoldHydra80 <91525267+ManifoldHydra80@users.noreply.github.com>
2026-04-26 21:40:35 +1000

    Nevermind it's working

--- adwarning/script/main/1.js
+++ adwarning/script/main/1.js
@@ -1,9 +1,13 @@
-async function runscript(){
+sync function runscript(){
+	if (document.referrer == 'https://l.j-ing.me'){
 		CurrentURL = new URL(location.href);
 		NewURL = await CurrentURL.searchParams.get('redirect-to')
 			if (localStorage.getItem('Skipadwarn')=='yes'){
 				location.replace(NewURL)
 			}
+	} else {
+		location.replace('https://j-ing.me')
+	}
 }
 ;
 function visitpage(){
