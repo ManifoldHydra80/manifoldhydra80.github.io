cddac5870d24a68dac4ba554252d1871b1843efd
ManifoldHydra80 <91525267+ManifoldHydra80@users.noreply.github.com>
2026-04-26 20:10:23 +1000

    Create 1.js

---
+++ adwarning/script/main/1.js
@@ -0,0 +1,21 @@
+async function runscript(){
+	if (document.referrer == 'https://j-ing.me'){
+		CurrentURL = new URL(location.href);
+		NewURL = await CurrentURL.searchParams.get('redirect-to')
+			if (localStorage.getItem('Skipadwarn')=='yes'){
+				location.replace(NewURL)
+			}
+	} else {
+		location.replace('https://j-ing.me')
+	}
+}
+;
+function visitpage(){
+	location.replace(NewURL)
+}
+;
+function visitpagenofuturewarn(){
+	localStorage.setItem('Skipadwarn','yes');
+	location.replace(NewURL)
+}
+runscript()
