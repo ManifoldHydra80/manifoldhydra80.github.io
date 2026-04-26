dc82aa6e9636357605e4f7c11b604a7bbab4e254
ManifoldHydra80 <91525267+ManifoldHydra80@users.noreply.github.com>
2026-04-26 21:12:57 +1000

    Removed the Referrer because it won't work

--- adwarning/script/main/1.js
+++ adwarning/script/main/1.js
@@ -1,13 +1,9 @@
 async function runscript(){
-	if (document.referrer == 'https://l.j-ing.me'){
 		CurrentURL = new URL(location.href);
 		NewURL = await CurrentURL.searchParams.get('redirect-to')
 			if (localStorage.getItem('Skipadwarn')=='yes'){
 				location.replace(NewURL)
 			}
-	} else {
-		location.replace('https://j-ing.me')
-	}
 }
 ;
 function visitpage(){
